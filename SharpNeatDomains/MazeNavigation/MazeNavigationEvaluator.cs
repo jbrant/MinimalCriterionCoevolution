2d51e06d5fbc4cb4430eca8799fc726296b472d0
Jonathan Brant <jonathan.charles.brant@gmail.com>
2015-08-08 17:00:38 -0400

    Started adding code for maze navigation experiments.

---
+++ SharpNeatDomains/MazeNavigation/MazeNavigationEvaluator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpNeat.Core;
+using SharpNeat.Phenomes;
+
+namespace SharpNeat.Domains.MazeNavigation
+{
+    class MazeNavigationEvaluator : IPhenomeEvaluator<IBlackBox>
+    {
+        #region Class Variables
+
+        // Evaluator state
+        private ulong _evaluationCount;
+        
+        #endregion
+
+        public ulong EvaluationCount
+        {
+            get { return _evaluationCount; }
+        }
+
+        public bool StopConditionSatisfied
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public FitnessInfo Evaluate(IBlackBox phenome)
+        {
+
+            // Increment eval count
+            _evaluationCount++;
+
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
