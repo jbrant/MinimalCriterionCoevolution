c85833c04bd1309399f1b909bc311ffc41ef05ec
jbrant <jonathan.charles.brant@gmail.com>
2015-08-11 01:49:28 -0400

    Added radar array implementation.

--- SharpNeatDomains/MazeNavigation/Components/PieSliceSensorArray.cs
+++ SharpNeatDomains/MazeNavigation/Components/PieSliceSensorArray.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace SharpNeat.Domains.MazeNavigation.Components
 {
     internal class PieSliceSensorArray
     {
+        internal PieSliceSensorArray()
+        {
+            Radars = new List<Radar>(4);
+
+            Radars.Add(new Radar(315, 405, 0));
+            Radars.Add(new Radar(45, 135, 0));
+            Radars.Add(new Radar(135, 225, 0));
+            Radars.Add(new Radar(225, 315, 0));
+        }
+
+        internal List<Radar> Radars { get; }
     }
 }
