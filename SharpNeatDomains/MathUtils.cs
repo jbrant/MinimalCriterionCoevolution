e7838b4e5cd9a87715511f4f181eadbcacdb6bcd
Jonathan Brant <jonathan.charles.brant@gmail.com>
2015-08-14 01:51:10 -0400

    Adding MathUtils file.

---
+++ SharpNeatDomains/MathUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SharpNeat.Domains
+{
+    public static class MathUtils
+    {
+        public static double toRadians(double angleInDegrees)
+        {
+            return (Math.PI/180)*angleInDegrees;
+        }
+
+        public static double toDegrees(double angleInRadians)
+        {
+            return (180/Math.PI)*angleInRadians;
+        }
+    }
+}
