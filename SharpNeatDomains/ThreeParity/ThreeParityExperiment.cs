5a7a3e56d19ce0bafe2af5a412eec9dc52a84acf
jbrant <jonathan.charles.brant@gmail.com>
2015-09-04 01:45:36 -0400

    Stubbed out 3-parity experiment.

---
+++ SharpNeatDomains/ThreeParity/ThreeParityExperiment.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using SharpNeat.Core;
+using SharpNeat.EvolutionAlgorithms;
+using SharpNeat.Genomes.Neat;
+using SharpNeat.Phenomes;
+
+namespace SharpNeat.Domains.ThreeParity
+{
+    class ThreeParityExperiment : INeatExperiment
+    {
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public int InputCount => 8;
+
+        public int OutputCount => 1;        
+
+        public int DefaultPopulationSize { get; private set; }        
+
+        public NeatEvolutionAlgorithmParameters NeatEvolutionAlgorithmParameters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public NeatGenomeParameters NeatGenomeParameters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Initialize(string name, XmlElement xmlConfig)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<NeatGenome> LoadPopulation(XmlReader xr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SavePopulation(XmlWriter xw, IList<NeatGenome> genomeList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IGenomeDecoder<NeatGenome, IBlackBox> CreateGenomeDecoder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IGenomeFactory<NeatGenome> CreateGenomeFactory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public INeatEvolutionAlgorithm<NeatGenome> CreateEvolutionAlgorithm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public INeatEvolutionAlgorithm<NeatGenome> CreateEvolutionAlgorithm(int populationSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public INeatEvolutionAlgorithm<NeatGenome> CreateEvolutionAlgorithm(IGenomeFactory<NeatGenome> genomeFactory, List<NeatGenome> genomeList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
