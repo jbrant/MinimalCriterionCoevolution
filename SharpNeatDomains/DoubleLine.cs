5eb00e152d0aa04acd106443f3a4f702a8410153
jbrant <jonathan.charles.brant@gmail.com>
2016-02-21 23:07:35 -0500

    Checking in bux fix for DoublePoint.CalculateLineIntersection.

--- SharpNeatDomains/DoubleLine.cs
+++ SharpNeatDomains/DoubleLine.cs
@@ -167,7 +167,9 @@ namespace SharpNeat.Domains
 
                 // Ensure that the intersection point actually lies within both line segments
                 if (xDeterminant >= Math.Min(a.Start.X, a.End.X) && xDeterminant <= Math.Max(a.Start.X, a.End.X) &&
-                    xDeterminant >= Math.Min(b.Start.X, b.End.X) && xDeterminant <= Math.Max(b.Start.X, b.End.X))
+                    xDeterminant >= Math.Min(b.Start.X, b.End.X) && xDeterminant <= Math.Max(b.Start.X, b.End.X) &&
+                    yDeterminant >= Math.Min(a.Start.Y, a.End.Y) && yDeterminant <= Math.Max(a.Start.Y, a.End.Y) &&
+                    yDeterminant >= Math.Min(b.Start.Y, b.End.Y) && yDeterminant <= Math.Max(b.Start.Y, b.End.Y))
                 {
                     intersectionFound = true;
                     return new DoublePoint(xDeterminant, yDeterminant);
