06870b3087c10c1df8b41ca9bc92e101592b19b8
jbrant <jonathan.charles.brant@gmail.com>
2016-03-05 02:25:07 -0500

    Completed bread-first recursive division implementation and got scaled up image generation working.

---
+++ MazeGenerationTester/RecursiveDivision/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeGenerationTester.RecursiveDivision
+{
+    public enum Direction
+    {
+        South = 1,
+        East
+    }
+}
---
+++ MazeGenerationTester/RecursiveDivision/Line2D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeGenerationTester.RecursiveDivision
+{
+    public class Line2D
+    {
+        public Line2D()
+        {
+        }
+
+        public Line2D(int xStart, int yStart, int xEnd, int yEnd)
+        {
+            StartPoint = new Point(xStart, yStart);
+            EndPoint = new Point(xEnd, yEnd);
+        }
+
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+    }
+}
