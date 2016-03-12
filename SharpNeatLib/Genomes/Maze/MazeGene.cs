5567ccc19d7adf10e9a6221b0df6b39dcf9f8f2f
jbrant <jonathan.charles.brant@gmail.com>
2016-03-12 00:57:17 -0500

    Missed checking in MazeGene.

---
+++ SharpNeatLib/Genomes/Maze/MazeGene.cs
@@ -0,0 +1,16 @@
+﻿namespace SharpNeat.Genomes.Maze
+{
+    public class MazeGene
+    {
+        public MazeGene(double wallLocation, double passageLocation, bool orientationSeed)
+        {
+            WallLocation = wallLocation;
+            PassageLocation = passageLocation;
+            OrientationSeed = orientationSeed;
+        }
+
+        public double WallLocation { get; set; }
+        public double PassageLocation { get; set; }
+        public bool OrientationSeed { get; }
+    }
+}
