fbe87b78dd9521e7f03109e4388786ba4a38d61d
jbrant <jonathan.charles.brant@gmail.com>
2016-03-19 14:47:11 -0400

    Added initial implementation of coevolution experiment interface and implementation.

---
+++ SharpNeatDomains/ICoevolutionExperiment.cs
@@ -0,0 +1,38 @@
+﻿#region
+
+using System.Xml;
+using SharpNeat.Core;
+
+#endregion
+
+namespace SharpNeat.Domains
+{
+    public interface ICoevolutionExperiment
+    {
+        /// <summary>
+        ///     Gets the name of the experiment.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        ///     Gets human readable explanatory text for the experiment.
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        ///     Gets the default first population size to use for the experiment.
+        /// </summary>
+        int DefaultPopulationSize1 { get; }
+
+        /// <summary>
+        ///     Gets the default second population size to use for the experiment.
+        /// </summary>
+        int DefaultPopulationSize2 { get; }
+
+        /// <summary>
+        ///     Initialize the experiment with some optional XML configutation data.
+        /// </summary>
+        void Initialize(string name, XmlElement xmlConfig, IDataLogger evolutionDataLogger = null,
+            IDataLogger evaluationDataLogger = null);
+    }
+}
---
+++ SharpNeatDomains/MazeNavigation/CoevolutionMCSExperiment/CoevolutionMazeNavigationMCSExperiment.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using SharpNeat.Core;
+using SharpNeat.EvolutionAlgorithms;
+using SharpNeat.Genomes.Maze;
+using SharpNeat.Genomes.Neat;
+
+namespace SharpNeat.Domains.MazeNavigation.CoevolutionMCSExperiment
+{
+    public class CoevolutionMazeNavigationMCSExperiment : ICoevolutionExperiment
+    {
+        #region Public Properties
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public int DefaultPopulationSize1 { get; private set; }
+
+        public int DefaultPopulationSize2 { get; private set; }
+
+        #endregion
+
+        #region Instance Variables
+
+        private NeatEvolutionAlgorithmParameters _neatEvolutionAlgorithmParameters;
+
+        private NeatGenomeParameters _neatGenomeParameters;
+
+        private MazeGenomeParameters _mazeGenomeParameters;
+
+        /// <summary>
+        ///     The maximum number of evaluations allowed (optional).
+        /// </summary>
+        private ulong? MaxEvaluations;
+
+        /// <summary>
+        ///     The maximum number of generations allowed (optional).
+        /// </summary>
+        private int? MaxGenerations;
+
+        #endregion
+
+        #region Public Methods
+
+        public void Initialize(string name, XmlElement xmlConfig, IDataLogger evolutionDataLogger = null,
+            IDataLogger evaluationDataLogger = null)
+        {
+            // Set boiler plate properties
+            Name = name;
+            DefaultPopulationSize1 = XmlUtils.TryGetValueAsInt(xmlConfig, "PopulationSize1") ?? default(int);
+            DefaultPopulationSize2 = XmlUtils.TryGetValueAsInt(xmlConfig, "PopulationSize2") ?? default(int);
+            Description = XmlUtils.GetValueAsString(xmlConfig, "Description");
+
+            // Set the evolution/genome parameters
+            _neatEvolutionAlgorithmParameters = ExperimentUtils.ReadNeatEvolutionAlgorithmParameters(xmlConfig);
+            _neatGenomeParameters = ExperimentUtils.ReadNeatGenomeParameters(xmlConfig);
+            
+        }
+
+        #endregion
+    }
+}
