6b02ed7811e42c2ef01e2f1d67d7a6238a4dc06a
jbrant <jonathan.charles.brant@gmail.com>
2016-05-30 16:28:12 -0400

    Checking in updates to maze simulator project as well as new maze configurations.

---
+++ MazeSimulatorSupport/MazeSimulationGraphicsController.cs
@@ -0,0 +1,87 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using SharpNeat.Phenomes.Mazes;
+
+#endregion
+
+namespace MazeSimulatorSupport
+{
+    internal class MazeSimulationGraphicsController
+    {
+        private readonly int _mazeCanvasHeight;
+        private readonly int _mazeCanvasWidth;
+        private readonly SolidColorBrush _wallBrush;
+        private readonly int _wallThickness;
+
+        public MazeSimulationGraphicsController(int windowHeight, int windowWidth, SolidColorBrush lineBrush,
+            int lineThickness)
+        {
+            _mazeCanvasHeight = windowHeight;
+            _mazeCanvasWidth = windowWidth;
+            _wallBrush = lineBrush;
+            _wallThickness = lineThickness;
+        }
+
+        public List<Line> GetMazeWalls(List<MazeStructureWall> mazeStructureWalls, int evolvedMazeHeight,
+            int evolvedMazeWidth)
+        {
+            // Maze boundaries aren't serialized, so generate them based on dimensions
+            /*mazeWalls.Add(CreateMazeLine(0, 0, true));
+            mazeWalls.Add(CreateMazeLine(0, 0, false));
+            mazeWalls.Add(CreateMazeLine(0, _mazeCanvasHeight, true));
+            mazeWalls.Add(CreateMazeLine(_mazeCanvasWidth, 0, false));*/
+
+            // Determine scale coefficient for fitting to canvas
+            double canvasHeightFitCoefficient = (double) _mazeCanvasHeight/evolvedMazeHeight;
+            double canvasWidthFitCoefficient = (double) _mazeCanvasWidth/evolvedMazeWidth;
+
+            // Add the interior walls and return the list
+            return
+                mazeStructureWalls.Select(
+                    mazeStructureWall =>
+                        CreateMazeLine(mazeStructureWall, canvasHeightFitCoefficient, canvasWidthFitCoefficient))
+                    .ToList();
+        }
+
+        private Line CreateMazeLine(int xStart, int yStart, bool isHorizontal)
+        {
+            Line line = new Line
+            {
+                // Set line segment end points
+                X1 = xStart,
+                Y1 = yStart,
+                X2 = isHorizontal ? xStart + _mazeCanvasWidth : xStart,
+                Y2 = isHorizontal ? yStart : yStart + _mazeCanvasHeight,
+
+                // Set additional layout properties
+                Stroke = _wallBrush,
+                StrokeThickness = _wallThickness
+            };
+
+            return line;
+        }
+
+        private Line CreateMazeLine(MazeStructureWall mazeStructureWall, double heightCoefficient,
+            double widthCoefficient)
+        {
+            Line line = new Line
+            {
+                // Extract start/end points from the maze structure wall
+                X1 = mazeStructureWall.StartMazeStructurePoint.X*widthCoefficient,
+                Y1 = mazeStructureWall.StartMazeStructurePoint.Y*heightCoefficient,
+                X2 = mazeStructureWall.EndMazeStructurePoint.X*widthCoefficient,
+                Y2 = mazeStructureWall.EndMazeStructurePoint.Y*heightCoefficient,
+
+                // Set additional layout properties
+                Stroke = _wallBrush,
+                StrokeThickness = _wallThickness
+            };
+
+            return line;
+        }
+    }
+}
---
+++ MazeSimulatorSupport/MazeSimulationIOController.cs
@@ -0,0 +1,94 @@
+﻿#region
+
+using System.IO;
+using System.Xml;
+using SharpNeat.Decoders;
+using SharpNeat.Decoders.Maze;
+using SharpNeat.Decoders.Neat;
+using SharpNeat.Domains;
+using SharpNeat.Genomes.Maze;
+using SharpNeat.Genomes.Neat;
+using SharpNeat.Phenomes;
+using SharpNeat.Phenomes.Mazes;
+
+#endregion
+
+namespace MazeSimulatorSupport
+{
+    internal class MazeSimulationIOController
+    {
+        private MazeDecoder _mazeGenomeDecoder;
+        private NeatGenomeDecoder _navigatorGenomeDecoder;
+
+        public SimulatorExperimentConfiguration ReadExperimentConfigurationFile(string configurationFile)
+        {
+            XmlDocument xmlExperimentConfig = new XmlDocument();
+
+            // Parse experiment name out of configuration file
+            string experimentName = Path.GetFileNameWithoutExtension(configurationFile);
+
+            // Load experiment configuration file
+            xmlExperimentConfig.Load(configurationFile);
+
+            // Get the root element
+            XmlElement rootElement = xmlExperimentConfig.DocumentElement;
+
+            // Determine navigator ANN controller activation scheme
+            NetworkActivationScheme navigatorActivationScheme = ExperimentUtils.CreateActivationScheme(rootElement,
+                "Activation");
+
+            // Create a new navigator genome decoder based on the activation scheme
+            _navigatorGenomeDecoder = new NeatGenomeDecoder(navigatorActivationScheme);
+
+            // Read in maze properties
+            int mazeHeight = XmlUtils.GetValueAsInt(rootElement, "MazeHeight");
+            int mazeWidth = XmlUtils.GetValueAsInt(rootElement, "MazeWidth");
+            int mazeScaleMultiplier = XmlUtils.GetValueAsInt(rootElement, "MazeScaleMultiplier");
+
+            // Create a new maze genome decoder based on the maze size
+            _mazeGenomeDecoder = new MazeDecoder(mazeHeight, mazeWidth, mazeScaleMultiplier);
+
+            // Read experiment parameteres and return experiment configuration
+            return new SimulatorExperimentConfiguration(experimentName, mazeHeight, mazeWidth, mazeScaleMultiplier,
+                navigatorActivationScheme, XmlUtils.GetValueAsInt(rootElement, "MaxTimesteps"),
+                XmlUtils.GetValueAsInt(rootElement, "MinSuccessDistance"));
+        }
+
+        public IBlackBox ReadNavigatorGenomeFile(string navigatorGenomeFile)
+        {
+            NeatGenome navigatorGenome;
+
+            // Parse the NEAT genome file
+            using (XmlReader xmlReader = XmlReader.Create(navigatorGenomeFile))
+            {
+                // NOTE: the genome factory input/output counts are arbitary, but likely accurate
+                // Regardless, they don't make a difference because offspring are not produced during the simulation
+                navigatorGenome = NeatGenomeXmlIO.ReadSingleGenomeFromRoot(xmlReader, false,
+                    new NeatGenomeFactory(10, 2));
+            }
+
+            // Decode navigator genome to its network phenotype
+            IBlackBox navigatorAnn = _navigatorGenomeDecoder.Decode(navigatorGenome);
+
+            return navigatorAnn;
+        }
+
+        public MazeStructure ReadMazeGenomeFile(string mazeGenomeFile)
+        {
+            // TODO: Needs to return MazeStructure
+
+            MazeGenome mazeGenome;
+
+            // Parse the maze genome file
+            using (XmlReader xmlReader = XmlReader.Create(mazeGenomeFile))
+            {
+                mazeGenome = MazeGenomeXmlIO.ReadSingleGenomeFromRoot(xmlReader, new MazeGenomeFactory());
+            }
+
+            // Decode maze genome to a maze structure (phenotype)
+            MazeStructure mazeStructure = _mazeGenomeDecoder.Decode(mazeGenome);
+
+            return mazeStructure;
+        }
+    }
+}
