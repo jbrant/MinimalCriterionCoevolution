3d98b5d1d139af3a61f9d6c7a5d76e58cb3b39be
jbrant <jonathan.charles.brant@gmail.com>
2016-05-30 16:28:12 -0400

    Checking in updates to maze simulator project as well as new maze configurations.

--- MazeSimulator/MainWindow.xaml.cs
+++ MazeSimulator/MainWindow.xaml.cs
@@ -1,72 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region
+
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Converters;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using MazeSimulatorSupport;
+using Microsoft.Win32;
+
+#endregion
 
 namespace MazeSimulator
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Rectangle testRect;
+        private readonly SimulationInstanceDirector _simInstanceDirector;
+        private readonly Rectangle testRect;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            int height, width = 320;
+            // Instantiate the controllers
+            _simInstanceDirector = new SimulationInstanceDirector();
 
-            // Create maze walls
-            Line line1 = new Line();
-            line1.Stroke = Brushes.LightSteelBlue;
-            line1.X1 = 0;
-            line1.X2 = 700;
-            line1.Y1 = 0;
-            line1.Y2 = 0;
-            line1.StrokeThickness = 2;
+            // Configure experiment instance
+            _simInstanceDirector.SetupNewExecution((int) SimulationCanvas.Height, (int) SimulationCanvas.Width,
+                Brushes.LightSteelBlue, 2);
 
-            Line line2 = new Line();
-            line2.Stroke = Brushes.LightSteelBlue;
-            line2.X1 = 0;
-            line2.X2 = 0;
-            line2.Y1 = 0;
-            line2.Y2 = 700;
-            line2.StrokeThickness = 2;
-
-            Line line3 = new Line();
-            line3.Stroke = Brushes.LightSteelBlue;
-            line3.X1 = 700;
-            line3.X2 = 700;
-            line3.Y1 = 0;
-            line3.Y2 = 700;
-            line3.StrokeThickness = 2;
-
-            Line line4 = new Line();
-            line4.Stroke = Brushes.LightSteelBlue;
-            line4.X1 = 0;
-            line4.X2 = 700;
-            line4.Y1 = 700;
-            line4.Y2 = 700;
-            line4.StrokeThickness = 2;
-
-            SimulationCanvas.Children.Add(line1);
-            SimulationCanvas.Children.Add(line2);
-            SimulationCanvas.Children.Add(line3);
-            SimulationCanvas.Children.Add(line4);
+            // TODO: Technically, this stuff should only be called when maze genome file is referenced
+            // Get maze boundaries/walls
+            /*foreach (Line mazeWall in _mazeSimulationGraphicsController.GetMazeWalls())
+            {
+                SimulationCanvas.Children.Add(mazeWall);
+            }*/
 
             testRect = new Rectangle();
             testRect.Width = 20;
@@ -89,12 +60,7 @@ namespace MazeSimulator
 
         private void dispatchTimer_Tick(object sender, EventArgs e)
         {
-            Canvas.SetLeft(testRect, Canvas.GetLeft(testRect)+1);
-        }
-
-        private void OpenMenuItem_OnClick(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
+            Canvas.SetLeft(testRect, Canvas.GetLeft(testRect) + 1);
         }
 
         private void CloseMenuItem_OnClick(object sender, RoutedEventArgs e)
@@ -115,6 +81,89 @@ namespace MazeSimulator
         private void PauseMenuItem_OnClick(object sender, RoutedEventArgs e)
         {
             throw new NotImplementedException();
+        }
+
+        private void OpenDatabaseMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadNavigatorGenomeMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Create open file dialog and filter to XML files
+            OpenFileDialog openNavigatorGenomeFileDialog = new OpenFileDialog();
+            openNavigatorGenomeFileDialog.Filter = "XML files (*.xml) | *.xml";
+
+            if (openNavigatorGenomeFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    _simInstanceDirector.LoadMazeNavigator(openNavigatorGenomeFileDialog.FileName);
+                }
+                catch (Exception navGenomeLoadException)
+                {
+                    MessageBox.Show(string.Format("An error occurred while loading the navigator genome: {0}",
+                        navGenomeLoadException.Message), "Navigator Genome Load Error", MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void LoadMazeGenomeMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Create open file dialog and filter to XML files
+            OpenFileDialog openMazeGenomeFileDialog = new OpenFileDialog();
+            openMazeGenomeFileDialog.Filter = "XML files (*.xml) | *.xml";
+
+            if (openMazeGenomeFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    _simInstanceDirector.LoadMaze(openMazeGenomeFileDialog.FileName);
+
+                    // TODO: Need to add some code to display the maze here
+                    foreach (Line mazeWall in _simInstanceDirector.ConstructMazeWalls())
+                    {
+                        SimulationCanvas.Children.Add(mazeWall);
+                    }
+                }
+                catch (Exception mazeGenomeLoadException)
+                {
+                    MessageBox.Show(string.Format("An error occurred while loading the maze genome: {0}",
+                        mazeGenomeLoadException.Message), "Maze Genome Load Error", MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void LoadExperimentConfigurationMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Create open file dialog and filter to XML files
+            OpenFileDialog openExperimentConfigFileDialog = new OpenFileDialog {Filter = "XML files (*.xml) | *.xml"};
+
+            if (openExperimentConfigFileDialog.ShowDialog() == true)
+            {
+                // Trap any errors that occur and report up to UI
+                try
+                {
+                    // Read in experiment configuration
+                    _simInstanceDirector.LoadExperimentConfiguration(openExperimentConfigFileDialog.FileName);
+
+                    // If loading was successful, enable loading the navigator and maze genome files
+                    LoadNavigatorGenomeMenuItem.IsEnabled = true;
+                    LoadMazeGenomeMenuItem.IsEnabled = true;
+                }
+                catch (Exception expLoadException)
+                {
+                    MessageBox.Show(string.Format("An error occurred while loading experiment configuration: {0}",
+                        expLoadException.Message), "Experiment Configuration Load Error", MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+
+                    // Reset the navigator/maze genome load menu items to false
+                    LoadNavigatorGenomeMenuItem.IsEnabled = false;
+                    LoadMazeGenomeMenuItem.IsEnabled = false;
+                }
+            }
         }
     }
 }
---
+++ MazeSimulatorSupport/SimulationInstanceDirector.cs
@@ -0,0 +1,64 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using SharpNeat.Phenomes;
+using SharpNeat.Phenomes.Mazes;
+
+#endregion
+
+namespace MazeSimulatorSupport
+{
+    public class SimulationInstanceDirector
+    {
+        #region Controllers and utility class references
+
+        private MazeSimulationGraphicsController _mazeSimulationGraphicsController;
+        private MazeSimulationIOController _mazeSimulationIoController;
+
+        #endregion
+
+        #region Configuration/instance variables
+
+        private SimulatorExperimentConfiguration _simulatorExperimentConfiguration;
+        private IBlackBox _navigatorAnnController;
+        private MazeStructure _mazeStructure;
+
+        #endregion
+
+        #region Public control methods
+
+        public void SetupNewExecution(int canvasHeight, int canvasWidth, SolidColorBrush wallBrush, int wallThickness)
+        {
+            _mazeSimulationIoController = new MazeSimulationIOController();
+            _mazeSimulationGraphicsController = new MazeSimulationGraphicsController(canvasHeight, canvasWidth,
+                wallBrush, wallThickness);
+        }
+
+        public void LoadExperimentConfiguration(string experimentConfigurationFile)
+        {
+            // Note that this will throw an exception up to the caller if configuration file read fails
+            _simulatorExperimentConfiguration =
+                _mazeSimulationIoController.ReadExperimentConfigurationFile(experimentConfigurationFile);
+        }
+
+        public void LoadMazeNavigator(string navigatorGenomeFile)
+        {
+            _navigatorAnnController =
+                _mazeSimulationIoController.ReadNavigatorGenomeFile(navigatorGenomeFile);
+        }
+
+        public void LoadMaze(string mazeGenomeFile)
+        {
+            _mazeStructure = _mazeSimulationIoController.ReadMazeGenomeFile(mazeGenomeFile);
+        }
+
+        public List<Line> ConstructMazeWalls()
+        {            
+            return _mazeSimulationGraphicsController.GetMazeWalls(_mazeStructure.Walls, _simulatorExperimentConfiguration.MazeHeight, _simulatorExperimentConfiguration.MazeWidth);
+        }
+
+        #endregion
+    }
+}
